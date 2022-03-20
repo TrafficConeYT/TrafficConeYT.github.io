0ace8314312a93f249cd1834bbcc80186e361bfb
TrafficConeYT <64033333+TrafficConeYT@users.noreply.github.com>
2022-03-20 01:08:45 -0400

    adding ffr stuff
    will it work?

---
+++ ffr-grabbag/t-xmlfeedbacktest.php
@@ -0,0 +1,10 @@
+<song genre="1">
+<songname>Test (Friday Night Funkin)</songname>
+<songauthor>MtH</songauthor>
+<songauthorurl></songauthorurl>
+<songdifficulty>1</songdifficulty>
+<songstyle>other</songstyle>
+<songlength>1:43</songlength>
+<songstepauthor></songstepauthor>
+<level>2HMJa</level>
+</song>
